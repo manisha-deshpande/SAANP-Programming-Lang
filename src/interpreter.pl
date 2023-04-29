3f2c008c924a86d570b3b1a7a28b6c45882d3e60
hauntarl <sameer.mungole@gmail.com>
2023-04-29 01:30:25 -0700

    read tokens from temp file

--- src/interpreter.pl
+++ src/interpreter.pl
@@ -1,8 +1,11 @@
 :- include(parser).
 :- include(eval).
 
-interpret(PROGRAM) :-
+interpret :-
     pack_install(regex),
+    open('saanp', read, FILE),
+    read(FILE, PROGRAM),
+    close(FILE),
     prog(PARSE_TREE, PROGRAM, []),
     eval_prog(PARSE_TREE, [], ENV),
     write('ENVIRONMENT: '),
